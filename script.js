b56fc95ee337a9bba265eb9f87e626fc0d1798c8
bosro <misterbosro@gmail.com>
2022-10-29 19:42:05 +0000

    second commit

---
+++ script.js
@@ -0,0 +1,33 @@
+const bar = document.getElementById('bar');
+
+const navbar = document.getElementById('navbar');
+
+
+
+if(bar) {
+  bar.addEventListener('click', () =>{
+      nav.classList.add('active');
+  })
+    
+  }
+
+
+if(close) {
+  close.addEventListener('click', () =>{
+      nav.classList.remove('active');
+  })
+    
+  }
+ 
+
+
+
+
+
+
+
+
+
+
+
+
